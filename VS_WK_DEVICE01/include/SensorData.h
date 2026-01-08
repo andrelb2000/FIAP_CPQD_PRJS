2003b7d36abaaf72b931a443cf1ade3cbe11da41
Prof. André Luiz Braga, D.Sc <andre.luiz.braga2000@gmail.com>
2026-01-08 11:23:06 -0300

    sdfsf

--- VS_WK_DEVICE01/include/SensorData.h
+++ VS_WK_DEVICE01/include/SensorData.h
@@ -10,20 +10,14 @@ public:
   String label;
   std::vector<double> temps;
   std::vector<double> gasLevels;
-
   SensorData();
   SensorData(const String &lbl);
-
   // Adiciona um único par (temperatura, gas) aos vetores internos.
   void add(double temp, double gas);
-
   // Retorna a média das temperaturas; 0.0 se vazio.
   double avgTemp() const;
-
   // Retorna a média dos níveis de gás; 0.0 se vazio.
   double avgGas() const;
-
   void clear();
 };
-
 #endif // SENSORDATA_H
